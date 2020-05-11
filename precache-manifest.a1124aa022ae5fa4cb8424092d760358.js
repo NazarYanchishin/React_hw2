20ab4ad88e681ab51382fca6ed4e2ee0569db0e6
Nazar <yanchishin1@gmail.com>
2020-05-11 14:16:58 +0300

    Updates

---
+++ precache-manifest.a1124aa022ae5fa4cb8424092d760358.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8c8a7e3629f5564f6c5a96ba5155166",
+    "url": "/React_hw2/index.html"
+  },
+  {
+    "revision": "5ab757a88a6103a5773b",
+    "url": "/React_hw2/static/css/main.7cb3bb86.chunk.css"
+  },
+  {
+    "revision": "581abf65a1e4eac29db8",
+    "url": "/React_hw2/static/js/2.72514b75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_hw2/static/js/2.72514b75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ab757a88a6103a5773b",
+    "url": "/React_hw2/static/js/main.0ac370d9.chunk.js"
+  },
+  {
+    "revision": "73b409afcad5d6ccdee6",
+    "url": "/React_hw2/static/js/runtime-main.e7d45025.js"
+  }
+]);
